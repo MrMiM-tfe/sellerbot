1fa5debaf1279b4430c69514b80ab81c508e76d4
MrMiM <mahdikhakbaz7@gmail.com>
2022-09-07 19:47:36 +0430

    edit bot

--- bot.js
+++ bot.js
@@ -1,27 +1,60 @@
-const mineflayer  = require("mineflayer")
+const mineflayer = require("mineflayer")
+const config = require('./config.json')
 
-const bot = mineflayer.createBot({
-    username: "MrMiM",
-    host: "play.paradise-city.ir",
-    version: "1.16.5"
-})
+const Login = (bot) => {
+    setTimeout(() => {
+        bot.chat('/login mahdi9909')
+    }, 5000);
+    bot.once('spawn', () => {
+        bot.once('spawn', () => {
+            bot.chat('/survival')
+            bot.once('spawn', () => {
 
-bot.once('spawn' ,() => {
-    console.log('done');
-    bot.once('spawn' ,() => {
-        bot.chat('/survival')
+            })
+        })
     })
-    
+}
 
-})
-setTimeout(() => {
-    bot.chat('/login mahdi9909')
-    
-}, 5000);
+const ManageChat = (bot) => {
+    bot.on("chat", (username, message) => {
 
-bot.on('chat' , (user ,msg) => {
-    console.log(msg);
-})
+        // Check if chat Log is true in the config.json
+        if (bot.config.chatLog) {
+            console.log(username, "||", message);
+        }
 
-bot.on('kicked', console.log)
-bot.on('error', console.log)
+        // Run commands
+        const command = message.split(" ")
+        console.log(command);
+        if (config.admins.includes(username)) { // Check if message is from admins
+            switch (command[1]) {
+
+                // put items in trash Dispenser
+                case "trash":
+                    trash(bot)
+                    break;
+
+                // log inventory items
+                case "tp":
+                    bot.chat("/tpa plusboy15")
+                    break
+            }
+        }
+    })
+}
+
+const createBot = () => {
+    const bot = mineflayer.createBot({
+        username: config.bot_info.username,
+        host: config.host,
+        version: config.version
+    })
+
+    Login(bot)
+    ManageChat(bot)
+
+    bot.on('kicked', console.log)
+    bot.on('error', console.log)
+}
+
+createBot()
