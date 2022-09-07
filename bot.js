6b04f875126e449975847c441724ab9e99a1798a
MrMiM <mahdikhakbaz7@gmail.com>
2022-09-07 19:06:49 +0430

    modified:   bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 const mineflayer  = require("mineflayer")
 
 const bot = mineflayer.createBot({
-    username: "plusboy15",
+    username: "MrMiM",
     host: "play.paradise-city.ir",
     version: "1.16.5"
 })
 
 bot.once('spawn' ,() => {
     console.log('done');
+    bot.chat('/login mahdi9909')
 })
 
 bot.on('chat' , (user ,msg) => {
